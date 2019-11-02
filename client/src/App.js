be47351433f368248e078bc85c5a3d3a226fcba7
Anand Chaudhary <anand92490@gmail.com>
2019-11-01 23:53:57 -0500

    Search Form Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
+// import SaveBooks from "./pages/SaveBooks";
+// import SearchBooks from "./pages/SearchBooks";
+// import NoMatch from "./pages/NoMatch"
 import "./App.css";
 
 class App extends Component {
