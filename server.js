661a891309c88cb3a0b40d57122cfe8f888015c8
Anand Chaudhary <anand92490@gmail.com>
2019-11-03 13:59:38 -0600

    created origin branch1 from branch goodstate coz i was plagued with'npm ERR! code ELIFECYCLE'

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds241288.mlab.com:41288/heroku_5k5t72h1");
 
 // Start the API server
 app.listen(PORT, function() {
