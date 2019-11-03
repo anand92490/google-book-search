5cc478f81c75d81fcf9d4ce9cc365b5b5dc18132
Anand Chaudhary <anand92490@gmail.com>
2019-11-02 20:31:14 -0500

    Yarn build for static apps

---
+++ client/build/precache-manifest.770e49794bbd376a0abe68d73894297e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "90febcb5a71139f30b0452dbfc2197fb",
+    "url": "/static/media/googlebooks.90febcb5.jpeg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "60ecb5dc525f2a6e8675",
+    "url": "/static/js/main.2ac37666.chunk.js"
+  },
+  {
+    "revision": "42aaa93e58d00bf9f392",
+    "url": "/static/js/2.e138253e.chunk.js"
+  },
+  {
+    "revision": "60ecb5dc525f2a6e8675",
+    "url": "/static/css/main.416c8297.chunk.css"
+  },
+  {
+    "revision": "cb1d0a7eb010b4d66bc9c626d00daf43",
+    "url": "/index.html"
+  }
+];
